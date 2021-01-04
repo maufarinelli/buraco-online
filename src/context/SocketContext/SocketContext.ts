f77df28613636348e0b0e38379407d4be115c82e
Mauricio Farinelli <mauricio.farinelli@autodesk.com>
2021-01-04 17:23:44 -0500

    prod server

--- src/context/SocketContext/SocketContext.ts
+++ src/context/SocketContext/SocketContext.ts
@@ -1,7 +1,8 @@
 import React from "react";
 import socketIOClient from "socket.io-client";
 
-const SOCKET_ENDPOINT = "ws://127.0.0.1:8000/";
+// const SOCKET_ENDPOINT = "ws://127.0.0.1:8000/";
+const SOCKET_ENDPOINT = "ws://buraco-online-be.herokuapp.com/";
 
 export const socket = socketIOClient(SOCKET_ENDPOINT, {
   transports: ["websocket"],
